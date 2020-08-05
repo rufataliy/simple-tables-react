b94871038244ca2359f04e804f51530e2b2ddf97
Rufat Aliyev <rufataliy@users.noreply.github.com>
2020-08-05 00:50:40 -0400

    Added error for wrong options object

--- src/utils/getHeaders.ts
+++ src/utils/getHeaders.ts
@@ -4,7 +4,11 @@ type GetHeaders = (options: Options<any>) => string[]
 
 
 export const getHeaders: GetHeaders = (options) => {
-    if (!options || !options.cellOptions) return []
+    if (!options || !options.cellOptions) {
+        throw Error(`Please provide options object with cellOptions configuration. 
+                    For more info see examples in https://github.com/rufataliy/simple-react-table.
+                    Using typescript could be handy to not miss required props.`)
+    }
     const headers = Object.keys(options.cellOptions).map(
         (option) => options.cellOptions[option]?.displayName ?? ''
     )
